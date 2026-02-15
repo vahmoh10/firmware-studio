7445433f2efd3654371c52f3a25e0baf495e4260
Vahid Mohammadi <vahmoh10@protonmail.com>
2026-02-16 00:56:01 +0100

    chore(firmware): add microcontroller

--- firmware/configuration/interface/link_options.cmake
+++ firmware/configuration/interface/link_options.cmake
@@ -1,10 +1,25 @@
 # SPDX-FileCopyrightText: 2026 Vahid Mohammadi vahmoh10@protonmail.com
 # SPDX-License-Identifier: AGPL-3.0-only
 
+if(NOT DEFINED MICROCONTROLLER)
+    message(FATAL_ERROR "MICROCONTROLLER is not defined")
+endif()
+
+set(SUPPORTED_MICROCONTROLLERS
+    "rp2350"
+)
+
+if(NOT MICROCONTROLLER IN_LIST SUPPORTED_MICROCONTROLLERS)
+    message(FATAL_ERROR "${MICROCONTROLLER} is not supported")
+endif()
+
 add_library(link_options INTERFACE)
 
 if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
-    set(LINK_OPTIONS -nostdlib)
+    set(LINK_OPTIONS
+        -T"${CMAKE_CURRENT_SOURCE_DIR}/implementation/microcontroller/${MICROCONTROLLER}/linker.ld"
+        -nostdlib
+    )
 else()
     message(FATAL_ERROR "${CMAKE_C_COMPILER_ID} is not supported")
 endif()
